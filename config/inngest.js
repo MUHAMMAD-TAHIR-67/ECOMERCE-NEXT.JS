a08f6025167e2f3aaa11465e7fa87142857a8753
Muhammad-Tahir-67 <tahirnaeem036@gmail.com>
2025-07-20 11:45:11 +0500

    first commit

--- config/inngest.js
+++ config/inngest.js
@@ -1,55 +1,75 @@
 import { Inngest } from "inngest";
 import { connectDB } from './mongoDB.js';
 import userModel from '../models/userModel.js';
+
 // Create a client to send and receive events
 export const inngest = new Inngest({ id: "next.js" });
-export const syncUser = inngest.createFunction({
-    id:'sync-user-from-clerk'
-},
-{
-    event:'clerk/user.created'
-},
-async ({event})=>{
-    const {id,first_name,last_name,email_addresses,image_url}=event.data
-    const userData={
-        _id:id,
-        email:email_addresses[0].email_address,
-        name:first_name+' '+last_name,
-        image_url:image_url
-        }
-        await connectDB()
-        await userModel.create(userData)
-}
-)
-// function to update  user data 
-export const syncuserupdate=inngest.createFunction({
-id:'update-user-from-clerk'
-},
-{
-    event:"clerk/user.updated"
-},
-async({event})=>{
-        const {id,first_name,last_name,email_addresses,image_url}=event.data
-    const userData={
-        _id:id,
-        email:email_addresses[0].email_address,
-        name:first_name+' '+last_name,
-        image_url:image_url
-        }
-        await connectDB()
-        await userModel.findByIdAndUpdate(id,userData)
-}
-)
-// inngesst gunction delete 
-export const deleteUser=inngest.createFunction({
-    id:"delete-user-with-cleark"
-},
-{
-    event:"clerk/user.deleted"
-},
-async({event})=>{
-           const {id,first_name,last_name,email_addresses,image_url}=event.data
-           await connectDB()
-           await userModel.findByIdAndDelete(id)
-}
-)
+
+export const syncUser = inngest.createFunction(
+  {
+    id: 'sync-user-from-clerk'
+  },
+  {
+    event: 'clerk/user.created'
+  },
+  async ({ event }) => {
+    try {
+      const { id, first_name, last_name, email_addresses, image_url } = event.data;
+      const userData = {
+        _id: id,
+        email: email_addresses[0].email_address,
+        name: `${first_name} ${last_name}`,
+        image_url
+      };
+      await connectDB();
+      await userModel.create(userData);
+    } catch (error) {
+      console.error("syncUser error:", error);
+      throw error;
+    }
+  }
+);
+
+export const syncuserupdate = inngest.createFunction(
+  {
+    id: 'update-user-from-clerk'
+  },
+  {
+    event: "clerk/user.updated"
+  },
+  async ({ event }) => {
+    try {
+      const { id, first_name, last_name, email_addresses, image_url } = event.data;
+      const userData = {
+        _id: id,
+        email: email_addresses[0].email_address,
+        name: `${first_name} ${last_name}`,
+        image_url
+      };
+      await connectDB();
+      await userModel.findByIdAndUpdate(id, userData);
+    } catch (error) {
+      console.error("syncuserupdate error:", error);
+      throw error;
+    }
+  }
+);
+
+export const deleteUser = inngest.createFunction(
+  {
+    id: "delete-user-with-clerk"
+  },
+  {
+    event: "clerk/user.deleted"
+  },
+  async ({ event }) => {
+    try {
+      const { id } = event.data;  // Only id is available in deleted event
+      await connectDB();
+      await userModel.findByIdAndDelete(id);
+    } catch (error) {
+      console.error("deleteUser error:", error);
+      throw error;
+    }
+  }
+);
--- config/mongoDB.js
+++ config/mongoDB.js
@@ -1,11 +1,23 @@
 import mongoose from "mongoose";
 
-export const connectDB = async () => {
-  try {
-    const conn = await mongoose.connect(`${process.env.MONGODB_URI}/next.js`);
-    console.log(`MongoDB connected: ${conn.connection.host}`);
-  } catch (error) {
-    console.error(`MongoDB connection error: ${error.message}`);
-    process.exit(1); 
+let cached = global.mongoose;
+
+if (!cached) {
+  cached = global.mongoose = { conn: null, promise: null };
+}
+
+export async function connectDB() {
+  if (cached.conn) {
+    return cached.conn;
   }
-};
+  
+  if (!cached.promise) {
+    cached.promise = mongoose.connect(process.env.MONGODB_URI).then(mongoose => {
+      return mongoose;
+    });
+  }
+  
+  cached.conn = await cached.promise;
+  console.log("MongoDB connected:", cached.conn.connection.host);
+  return cached.conn;
+}
